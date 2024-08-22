64b51d5019a0f1aff2dac353f4702e7104df2aaf
YoungGreek <winter.321@mail.ru>
2024-08-22 16:53:50 +0700

    Initial commit

--- Change numeral system/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/YoungGreek/CLionProjects/lab0/lab0
-# Build directory: C:/Users/YoungGreek/CLionProjects/lab0/lab0/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(testlab0.exe "C:/Users/YoungGreek/CLionProjects/lab0/lab0/test/testlab0.exe" "./lab0")
-set_tests_properties(testlab0.exe PROPERTIES  DEPENDS "lab0" _BACKTRACE_TRIPLES "C:/Users/YoungGreek/CLionProjects/lab0/modules/test_runner.cmake;23;add_test;C:/Users/YoungGreek/CLionProjects/lab0/modules/test_runner.cmake;0;;C:/Users/YoungGreek/CLionProjects/lab0/modules/common_lab.cmake;3;include;C:/Users/YoungGreek/CLionProjects/lab0/modules/common_lab.cmake;0;;C:/Users/YoungGreek/CLionProjects/lab0/lab0/CMakeLists.txt;9;include;C:/Users/YoungGreek/CLionProjects/lab0/lab0/CMakeLists.txt;0;")
